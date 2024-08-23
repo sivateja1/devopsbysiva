872d130d9ee8ddbedc4ee3cfbff9c21787f6853b
Sannidhi Siva Teja <sivateja152@gmail.com>
2024-08-23 17:49:31 +0530

    modified

--- coversation.sh
+++ coversation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Author siva
-person1 = siva
-person2 = virat
+person1=siva
+person2=virat
 echo "$person1:: Hi $person2"
 echo "$person2:: Hi $person1"
 echo "$person1:: how are you?"
