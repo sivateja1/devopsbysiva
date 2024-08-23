cfb0d2ac24f09a3465405b899e17268cb22cfb59
Sannidhi Siva Teja <sivateja152@gmail.com>
2024-08-23 17:45:55 +0530

    variables

---
+++ coversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Author siva
+person1 = siva
+person2 = virat
+echo "$person1:: Hi $person2"
+echo "$person2:: Hi $person1"
+echo "$person1:: how are you?"
+echo "$person2:: i am fine"
