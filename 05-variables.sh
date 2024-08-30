7212c9ddf4a7c020e3c5c693ab34b5d0cc65a003
Sannidhi Siva Teja <sivateja152@gmail.com>
2024-08-30 19:32:40 +0530

    removed :

--- 05-variables.sh
+++ 05-variables.sh
@@ -6,7 +6,7 @@ read USERNAME #takes input into USERNAME variable
 
 echo "Username entered is: $USERNAME"
 
-echo "Please enter your password::"
+echo "Please enter your password:"
 
 read -s PASSWORD
 echo "Password entered is: $PASSWORD"
