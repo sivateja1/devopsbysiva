ee81bfa315cb8afabdcf2a36acd77d166920f1c5
Sannidhi Siva Teja <sivateja152@gmail.com>
2024-08-30 19:35:37 +0530

    removed :

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,8 +2,8 @@
 PERSON1=SIVA
 PERSON2=TEJA
 
-echo "$PERSON1:: Hi ${PERSON2}, How are you?"
-echo "${PERSON2}:: Hello $PERSON1. I am fine. How are you doing?"
-echo "$PERSON1:: I am doing good ${PERSON2}. What's going on?"
-echo "${PERSON2}:: I started learning Shell Script $PERSON1"
+echo "$PERSON1: Hi ${PERSON2}, How are you?"
+echo "${PERSON2}: Hello $PERSON1. I am fine. How are you doing?"
+echo "$PERSON1: I am doing good ${PERSON2}. What's going on?"
+echo "${PERSON2}: I started learning Shell Script $PERSON1"
 
