39e97a91285b99c5a70b199060673a42471ce106
Sannidhi Siva Teja <sivateja152@gmail.com>
2024-09-11 19:01:52 +0530

    ok

--- Terraform/count/ec2.tf
+++ Terraform/count/ec2.tf
@@ -1,12 +1,12 @@
-resource "aws_instance" "terraform" {
-
+resource "aws_instance" "backend" {
+    count = length(var.instance_names)
     ami = "ami-09c813fb71547fc4f"
-    instance_type = var.environment == "prod" ? "t3.small" : "t3.micro"
+    instance_type = "t3.micro"
     vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
     tags = {
-        Name = "terraform"
+        Name = var.instance_names[count.index]
     }
-}       
+}         
 
 resource "aws_security_group" "allow_ssh_terraform" {
     name        = "allow_sshh"
