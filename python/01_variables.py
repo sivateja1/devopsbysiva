862096a4ddeb2cd701a8115dc912b29cf68c4dfb
Sannidhi Siva Teja <sivateja152@gmail.com>
2024-09-21 17:18:48 +0530

    added

---
+++ python/01_variables.py
@@ -0,0 +1,136 @@
+# -----------------------------------------------------------------------comments(#)  #
+# This is single line comment
+
+# -----------------------------------------------------------------------Multi line comment (""") #
+"""
+this is a multi line comment
+"""
+
+# -----------------------------------------------------------------------Defining a variable #
+# Define a variable
+a = 42 # This is a integer variable
+
+# -----------------------------------------------------------------------printing the value stored in variable #
+# Print the value that is stored inside the variable
+print(a)
+
+# ----------------------------------------------------------------------- Float #
+# Float value
+b = 42.25
+print(b)
+
+# -----------------------------------------------------------------------Boolean #
+# Boolean
+c = True
+
+# -----------------------------------------------------------------------Strings #
+# Strings
+
+d = "This is a string"
+e = 'This is also a string'
+f = """
+        This is a multi line string
+        Hello strings
+    """
+
+# -----------------------------------------------------------------------String "Example" #
+# Today's weather is nice
+d = "Today's weather is nice"
+print(d)
+
+# -----------------------------------------------------------------------List [" "]#
+# This is a list
+list = ["siva","Teja","sannidhi"]
+print(list)
+
+# -----------------------------------------------------------------------Tuple (" ") #
+# Tuple
+tuple = ("learn", "python", "siva")
+print(tuple)
+
+# ----------------------------------------------------------------------- Dict {' ': 1} #
+# Dict
+dict = {'a': 1, 'b': 2}
+print(dict)
+
+# -----------------------------------------------------------------------Set {' '," "} #
+# Set
+# consider the values in an arbitrary way
+
+set = {'a', 'b', "abc"}
+print(set)
+
+# -----------------------------------------------------------------------Type type() #
+# type() function -> prints the datatype of the variable
+print(type(dict))
+print(type(tuple))
+
+# ----------------------------------------------------------------------- #
+# Operations
+
+
+
+# Add
+a = 42
+b = 20.45
+c = a + b
+print(c)
+
+# Sub
+d = a - b
+print(d)
+
+# Multi
+e = a * b
+print(e)
+
+# Divide
+f = 12
+g = 3
+h = f / g
+print(h, type(h))
+
+
+# Integer division
+h = f // g
+print(h)
+
+
+# Modulo division
+i = f % g
+print(i)
+
+
+# Concatenation
+a = "42"
+b = "43"
+print(a + b) 
+
+# POWER
+a = 10
+print(a**2)
+
+# comparission operators
+a = 10
+b = 20
+res = a > b
+res_1 = a < b
+res_2 = a != b
+res_3 = a == b
+
+print(res, res_1, res_2, res_3)
+
+# Logical Operators
+
+#AND ,NOT, OR
+a = True
+b = False
+
+res = a and b
+res_1 = a or b
+res_2 = not a
+res_3 = not b
+print(res, res_1, res_2, res_3)
+
+
+
